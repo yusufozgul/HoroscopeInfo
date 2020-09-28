c0300e7ecd74364ab450e258e118dfe2ef79021c
Yusuf Özgül <yozgul@icloud.com>
2020-09-28 18:36:22 +0300

    Project Added

---
+++ HoroscopeInfo/HoroscopeInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HoroscopeInfo
+//
+//  Created by Yusuf Özgül on 28.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
